a18272621a2111b1f5299e2c85e9a0ee1e271bed
vritara24 <vritara24@gmail.com>
2026-03-14 14:02:22 +0530

    connect frontend to render backend

--- public/app.js
+++ public/app.js
@@ -1,4 +1,4 @@
-const API_BASE = "";
+const API_BASE = "https://vritara-backend.onrender.com";
 let currentLocation = { latitude: null, longitude: null };
 let otpEmailStore = "";
 let forgotEmailStore = "";
